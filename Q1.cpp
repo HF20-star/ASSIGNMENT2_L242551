076e04a5c0ac26a7e23f4af717e93d6bcac686f0
HF20-star <l242551@lhr.nu.edu.pk>
2025-10-22 19:36:01 +0500

    class Notepad and insertChar modified

--- Q1.cpp
+++ Q1.cpp
@@ -81,6 +81,56 @@ public:
 
 };
 
+class Notepad {
+public:
+	Node* head;
+	Node* tail;
+	Node* cursor;
+	Queue redoQueue;
+
+	Notepad()
+	{
+		head = nullptr;
+		tail = nullptr;
+		cursor = nullptr;
+	}
+
+	void insertChar(char d)
+	{
+		Node* newNode = new Node(d);
+		if (!head)
+		{
+			head = tail = newNode;
+			cursor = nullptr;
+		}
+		else if (cursor == head)
+		{
+			newNode->next = head;
+			head->prev = newNode;
+			head = newNode;	
+		}
+		else if (!cursor)
+		{
+			tail->next = newNode;
+			newNode->prev = tail;
+			tail = newNode;
+		}
+		else
+		{
+			newNode->prev = cursor->prev;
+			newNode->next = cursor;
+			cursor->prev->next = newNode;
+			cursor->prev = newNode;
+		}
+	}
+
+
+
+
+};
+
+
+
 
 
 
